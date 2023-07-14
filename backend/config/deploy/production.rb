94f8b2221b1c351c33edb51afa5aa157a7615f60
Hoang Duc Cong <conghd@gmail.com>
2023-07-14 17:04:55 -0600

    Updating capistrano tasks.

--- backend/config/deploy/production.rb
+++ backend/config/deploy/production.rb
@@ -3,11 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "34.127.7.253", user: "tradabk", roles: %w{app web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
+set :branch, "main"
 
 # role-based syntax
 # ==================
