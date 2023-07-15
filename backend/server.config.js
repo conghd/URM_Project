63d33e3673510ff1942ed4ac418ce6b3e83394af
Hoang Duc Cong <conghd@gmail.com>
2023-07-15 08:45:14 -0600

    Disabling pm2 autostart when files have changed.

--- backend/server.config.js
+++ backend/server.config.js
@@ -5,7 +5,7 @@ module.exports = {
         script: './server.js',
         instances: 1,
         exec_mode: 'fork',
-        watch: true,
+        watch: false, // If true, pm2 will restart when there is a file change, even log files
         env: {
           NODE_ENV: 'production',
           PORT: '8080'
